cdb1929bd7c7682745420d8b9bf0e4808bf7641d
yousradiab <yousserah@gmail.com>
2024-03-07 23:49:13 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,36 +13,38 @@ import CategoryForm from "./recipes/CategoryForm";
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/info" element={<Home />} />
-        <Route path="/categories/" element={<Categories />} />
-        <Route path="/recipes" element={<RecipesLayout />}>
-          <Route path=":id" element={<Recipe />} />
-        </Route>
+    
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Home />} />
+          <Route path="/categories/" element={<Categories />} />
+          <Route path="/recipes" element={<RecipesLayout />}>
+            <Route path=":id" element={<Recipe />} />
+          </Route>
 
-        <Route
-          path="/addcategories"
-          element={
-            <RequireAuth roles={["ADMIN"]}>
-              <CategoryForm />
-            </RequireAuth>
-          }
-        />
+          <Route
+            path="/addcategories"
+            element={
+              <RequireAuth roles={["ADMIN"]}>
+                <CategoryForm />
+              </RequireAuth>
+            }
+          />
 
-        <Route
-          path="/add"
-          element={
-            <RequireAuth roles={["USER"]}>
-              <RecipeForm />
-            </RequireAuth>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<h2>Not Found</h2>} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
-    </Layout>
+          <Route
+            path="/add"
+            element={
+              <RequireAuth roles={["USER"]}>
+                <RecipeForm />
+              </RequireAuth>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h2>Not Found</h2>} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </Layout>
+  
   );
 }
